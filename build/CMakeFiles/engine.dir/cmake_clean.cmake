950b25bd76c8844b1c66f100c553f2857bb73ddf
tealvl <70812161+tealvl@users.noreply.github.com>
2024-01-23 17:07:39 +0300

    del

---
+++ build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/main.cpp.obj"
+  "CMakeFiles/engine.dir/main.cpp.obj.d"
+  "engine.exe"
+  "engine.exe.manifest"
+  "engine.pdb"
+  "libengine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/engine.dir/main.cpp.obj.d
@@ -0,0 +1,18 @@
+CMakeFiles/engine.dir/main.cpp.obj: C:\Dev\cpp-projects\engine\main.cpp \
+ C:/Dev/cpp-projects/engine/dependencies/glfw/include/GLFW/glfw3.h \
+ C:/Dev/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stddef.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Dev/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdint.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Dev/MinGW/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h
