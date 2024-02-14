30062bbc022cf607c98fb10f7df5e4e7a76b6c8a
tealvl <70812161+tealvl@users.noreply.github.com>
2024-02-15 00:00:20 +0300

    Start set up pipeline

--- src/HelloTriangleApplication.hpp
+++ src/HelloTriangleApplication.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <optional>
-
+#include <fstream>
 
 const std::vector<const char*> validationLayers = {
 "VK_LAYER_KHRONOS_validation"
@@ -23,6 +23,21 @@ const bool enableValidationLayers = true;
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+    size_t fileSize = (size_t) file.tellg();
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+
+    return buffer;
+}
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -92,4 +107,6 @@ private:
     VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
     void createSwapChain();
     void createImageViews();
+    void createGraphicsPipeline();
+    VkShaderModule createShaderModule(const std::vector<char> &code);
 };
