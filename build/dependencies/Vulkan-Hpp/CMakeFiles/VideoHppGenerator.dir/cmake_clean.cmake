9897a8d33bba2800136a24e67280c06309522a89
tealvl <70812161+tealvl@users.noreply.github.com>
2024-04-12 23:24:09 +0300

    add stb

---
+++ build/dependencies/Vulkan-Hpp/CMakeFiles/VideoHppGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoHppGenerator.dir/VideoHppGenerator.cpp.obj"
+  "CMakeFiles/VideoHppGenerator.dir/VideoHppGenerator.cpp.obj.d"
+  "CMakeFiles/VideoHppGenerator.dir/tinyxml2/tinyxml2.cpp.obj"
+  "CMakeFiles/VideoHppGenerator.dir/tinyxml2/tinyxml2.cpp.obj.d"
+  "VideoHppGenerator.exe"
+  "VideoHppGenerator.exe.manifest"
+  "VideoHppGenerator.pdb"
+  "libVideoHppGenerator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoHppGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/dependencies/Vulkan-Hpp/CMakeFiles/VulkanHppGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanHppGenerator.dir/VulkanHppGenerator.cpp.obj"
+  "CMakeFiles/VulkanHppGenerator.dir/VulkanHppGenerator.cpp.obj.d"
+  "CMakeFiles/VulkanHppGenerator.dir/tinyxml2/tinyxml2.cpp.obj"
+  "CMakeFiles/VulkanHppGenerator.dir/tinyxml2/tinyxml2.cpp.obj.d"
+  "VulkanHppGenerator.exe"
+  "VulkanHppGenerator.exe.manifest"
+  "VulkanHppGenerator.pdb"
+  "libVulkanHppGenerator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanHppGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
