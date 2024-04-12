da7dafa6ae60524c9aeaa1def5df5e1c654f333f
tealvl <70812161+tealvl@users.noreply.github.com>
2024-04-12 23:27:17 +0300

    .

--- src/HelloTriangleApplication.hpp
+++ src/HelloTriangleApplication.hpp
@@ -16,13 +16,14 @@
 #include <chrono>
 
 #define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #include <glm/gtc/matrix_transform.hpp>
 
 
 struct UniformBufferObject {
-    glm::mat4 model;
-    glm::mat4 view;
-    glm::mat4 proj;
+    alignas(16) glm::mat4 model;
+    alignas(16) glm::mat4 view;
+    alignas(16) glm::mat4 proj;
 };
 
 struct Vertex {
